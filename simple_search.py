ee80af9c10fe7d827453e33064017fe4be69f689
Greg Brown <gregplaysguitar@gmail.com>
2010-08-20 12:11:53 +1200

    stopwords

--- simple_search.py
+++ simple_search.py
@@ -27,8 +27,14 @@ from django.conf import settings
 from django.core.exceptions import FieldError
 
 
+
+DEFAULT_STOPWORDS = 'a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your'
+
     
 class BaseSearchForm(forms.Form):
+    STOPWORD_LIST = DEFAULT_STOPWORDS.split(',')
+    DEFAULT_OPERATOR = Q.__and__
+    
     q = forms.CharField(label='Search', required=False)
     def clean_q(self):
         return self.cleaned_data['q'].strip()
@@ -43,8 +49,8 @@ class BaseSearchForm(forms.Form):
         abstract = True
         base_qs = None
         search_fields = None
-        default_operator = Q.__and__
-
+        
+        
     def get_text_search_query(self, query_string):
         filters = []
         
@@ -65,24 +71,27 @@ class BaseSearchForm(forms.Form):
                 return "%s__icontains" % field_name
         
         first = True
-        for bit in smart_split(query_string):
+        split_q = list(smart_split(query_string))
+        
+        # filter stopwords but only if there are useful words
+        filtered_q = []
+        for bit in split_q:
+            if bit not in self.STOPWORD_LIST:
+                filtered_q.append(bit)
+        for bit in (filtered_q if len(filtered_q) else split_q):
             or_queries = [Q(**{construct_search(str(field_name), first): bit}) for field_name in self.Meta.search_fields]
             filters.append(reduce(Q.__or__, or_queries))
             first = False
         
-        return reduce(getattr(self.Meta, 'default_operator', Q.__and__), filters)
+        if len(filters):
+            return reduce(self.DEFAULT_OPERATOR, filters)
+        else:
+            return False
+            
 
-
-    def construct_filter_args(self, cleaned_data=None):
+    def construct_filter_args(self, cleaned_data):
         args = []
-        
-        if not cleaned_data:
-            cleaned_data = self.cleaned_data.copy()
-        
-        # construct text search
-        if cleaned_data['q']:
-            args.append(self.get_text_search_query(cleaned_data.pop('q')))
-        
+                
         # if its an instance of Q, append to filter args
         # otherwise assume an exact match lookup
         for field in cleaned_data:
@@ -102,13 +111,21 @@ class BaseSearchForm(forms.Form):
     
         
     def get_result_queryset(self):
-        qs = self.Meta.base_qs.filter(*self.construct_filter_args())
-        """
-        for field_name in self.Meta.search_fields:
-            if '__' in field_name:
-                qs = qs.distinct()
-                break
-        """
+        qs = self.Meta.base_qs
+        cleaned_data = self.cleaned_data.copy()
+        
+        
+        # construct text search
+        if cleaned_data['q']:
+            text_q = self.get_text_search_query(cleaned_data.pop('q'))
+            if text_q:
+                qs = qs.filter(text_q)
+            else:
+                qs = qs.none()
+        
+        qs = qs.filter(*self.construct_filter_args(cleaned_data))
+
+
         if self.cleaned_data['order_by']:
             qs = qs.order_by(*self.cleaned_data['order_by'].split(','))
                             
