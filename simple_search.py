59f72013bb34910235625290aa172fd15966b3b3
Greg Brown <gregplaysguitar@gmail.com>
2012-09-10 11:51:44 +1200

    Reworked package

--- simple_search.py
+++ simple_search.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+__version__ = '1.0'
+VERSION = tuple(map(int, __version__.split('.')))
 
 import re
 from django.db.models import Q
--- simple_search/__init__.py
+++
@@ -1,2 +0,0 @@
-__version__ = '1.0'
-VERSION = tuple(map(int, __version__.split('.')))
